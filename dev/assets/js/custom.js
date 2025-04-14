8c15329832ea29d2f3a23cb5581eae6797101291
Lusina Akopian <lusina2490@gmail.com>
2025-04-14 14:46:28 +0400

    add madal

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -11,3 +11,62 @@ burger.addEventListener('click', ()=> {
     mobileContainer.classList.toggle('show')
 
 })
+
+
+// modal//
+
+const modalTrigers = document.querySelectorAll('[data-trigger-modal]');
+const modalClose = document.querySelectorAll('[data-modal-close]')
+const overlay = document.querySelector('.overlay')
+const body = document.querySelector('body')
+
+
+
+
+function showModal() {
+    
+    body.classList.add('overflow-hidden')
+    overlay.classList.add('show')
+    
+}
+
+function closeModal(){
+    const openModal = document.querySelector('.modal.show');
+    body.classList.remove('overflow-hidden')
+    overlay.classList.remove('show')
+
+    openModal.classList.remove('show')
+    
+
+}
+
+
+modalTrigers.forEach(item => {
+    item.addEventListener('click' , function(event){
+        event.preventDefault()
+
+        const dataTriger = item.getAttribute('data-trigger-modal')
+        const modal = document.querySelector('#' + dataTriger)
+        
+        showModal()
+        modal.classList.add('show')
+    })
+    
+})
+
+modalClose.forEach(close => {
+    close.addEventListener('click', closeModal)
+})
+
+// function closeModal() {
+    
+//     overlay.classList.remove('show')
+//     modal.classList.remove('show')
+//     body.classList.remove('overflow-hidden')
+    
+// }
+
+
+// btnTrigerProcess.addEventListener('click', showModal)
+// btnModalClose.addEventListener('click', closeModal)
+
